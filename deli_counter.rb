f1855a0f77ea5c9bea9c349a182a71af14f96340
Joshua M Loran <joshua.loran@gmail.com>
2019-01-09 21:44:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,14 @@
 # Write your code here.
-def line(line)
-  if (line.count == 0)
+def line(line_list)
+  if (line_list.count == 0)
     puts "The line is currently empty."
   end
+elsif 
+  line_position = 1
+  new_list = ["The line is currently: "]
+  line_list.each do |person|
+    new_list << "#{line_position}. #{person} "
+    line_position += 1
+  end
+  puts new_list    
 end
